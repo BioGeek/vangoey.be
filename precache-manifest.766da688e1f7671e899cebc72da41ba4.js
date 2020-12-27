416c1b342519127da8cc35545085fb740ef2db80
Jeroen Van Goey <jeroen.vangoey@gmail.com>
2020-12-27 15:40:44 +0100

    Updates

---
+++ precache-manifest.766da688e1f7671e899cebc72da41ba4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "342f65efdaefa78fac8430ae1350406c",
+    "url": "/vangoey.be/index.html"
+  },
+  {
+    "revision": "b104a18211bb5c67083b",
+    "url": "/vangoey.be/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "dc4071c01500a774ee05",
+    "url": "/vangoey.be/static/css/main.8d52491f.chunk.css"
+  },
+  {
+    "revision": "b104a18211bb5c67083b",
+    "url": "/vangoey.be/static/js/2.7b0f0dcd.chunk.js"
+  },
+  {
+    "revision": "01f1b5ae975a8d17e413a750f30bfaaa",
+    "url": "/vangoey.be/static/js/2.7b0f0dcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc4071c01500a774ee05",
+    "url": "/vangoey.be/static/js/main.b07bd4a3.chunk.js"
+  },
+  {
+    "revision": "059dd093b8972cbf03b7",
+    "url": "/vangoey.be/static/js/runtime-main.bcbd9a5f.js"
+  },
+  {
+    "revision": "18e7fad541604561c8e22a36a9185404",
+    "url": "/vangoey.be/static/media/Coolvetica.ttf.18e7fad5.eot"
+  },
+  {
+    "revision": "2872b1fc8b52158b1f0c4361061d366f",
+    "url": "/vangoey.be/static/media/Coolvetica.ttf.2872b1fc.svg"
+  },
+  {
+    "revision": "5bd516998e00831e7676715db5a71145",
+    "url": "/vangoey.be/static/media/Coolvetica.ttf.5bd51699.woff"
+  }
+]);
